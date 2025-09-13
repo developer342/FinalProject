bfd04f97be1fe4ac3b613face2c64c111668411c
Kim Minhyoung <kmhy14593@gmail.com>
2025-09-14 07:24:47 +0900

    내 정보 조회

---
+++ FinalServer/src/main/java/com/example/finalserver/users/dto/UserProfileResponse.java
@@ -0,0 +1,12 @@
+package com.example.FinalServer.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserProfileResponse {
+
+  private Long id;
+  private String email;
+}
